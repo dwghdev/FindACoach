94acaf79571ae110492890e1771d85bf3fda9ea6
Dale Walter <dalewaltergh@gmail.com>
2023-01-24 03:08:01 +0800

    add route page components

--- src/router.js
+++ src/router.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+
+import RequestReceive from './pages/requests/RequestReceive.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import NotFound from './pages/NotFound.vue';
+
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetails,
       children: [
         {
-          path: 'contact', component: null
+          path: 'contact',
+          component: ContactCoach
         } // /coaches/c1/contact
       ]
     },
-    { path: '/register', component: null },
-    { path: '/request', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoachRegistration },
+    { path: '/request', component: RequestReceive },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
